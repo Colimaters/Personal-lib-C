1d3f0c71c846b7e22aec3923ebf1f7434355299d
Darius MOREL <darius.morel@epitech.eu>
2020-08-31 11:12:05 +0200

    add frensh macro

--- include/macro.h
+++ include/macro.h
@@ -25,35 +25,40 @@
 #define FAILURE 84
 
 //Conditions
-#define si if                   /*!< To code in French */
-#define sinon else              /*!< To code in French */
-#define tantque while           /*!< To code in French */
-#define pour for                /*!< To code in French */
-#define faire do                /*!< To code in French */
-#define cas case                /*!< To code in French */
-#define commutateur switch      /*!< To code in French */
+#define si if                   /*!< Pour coder en français */
+#define sinon else              /*!< Pour coder en français */
+#define tantque while           /*!< Pour coder en français */
+#define pour for                /*!< Pour coder en français */
+#define faire do                /*!< Pour coder en français */
+#define cas case                /*!< Pour coder en français */
+#define commutateur switch      /*!< Pour coder en français */
 
 //Mots-clés
-#define retour return           /*!< To code in French */
-#define casser break            /*!< To code in French */
-#define continuer continue      /*!< To code in French */
-#define taillede sizeof         /*!< To code in French */
-#define statique static         /*!< To code in French */
-#define constant const          /*!< To code in French */
+#define retour return           /*!< Pour coder en français */
+#define casser break            /*!< Pour coder en français */
+#define continuer continue      /*!< Pour coder en français */
+#define taillede sizeof         /*!< Pour coder en français */
+#define definirtype typedef     /*!< Pour coder en français */
+#define nomdutype typename      /*!< Pour coder en français */
+#define structure struct        /*!< Pour coder en français */
+#define statique static         /*!< Pour coder en français */
+#define constant const          /*!< Pour coder en français */
 
 //Valeurs
-#define vrai true               /*!< To code in French */
-#define faux false              /*!< To code in French */
+#define vrai true               /*!< Pour coder en français */
+#define faux false              /*!< Pour coder en français */
 
 //Variables
-#define vide void               /*!< To code in French */
+#define vide void               /*!< Pour coder en français */
 
 //Opérateurs
-#define et &&                   /*!< To code in French */
-#define ou ||                   /*!< To code in French */
+#define et &&                   /*!< Pour coder en français */
+#define ou ||                   /*!< Pour coder en français */
+#define non !                   /*!< Pour coder en français */
+#define different !=            /*!< Pour coder en français */
 
 //Utils
-#define pourtoujours for (;;)   /*!< To code in French */
-#define boucleinfinie while (1) /*!< To code in French */
+#define pourtoujours for (;;)   /*!< Pour coder en français */
+#define boucleinfinie while (1) /*!< Pour coder en français */
 
 #endif /* MACRO_H_ */
