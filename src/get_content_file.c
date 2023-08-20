9cc40e9ef3b343d50225b3bedb257c41a8dab4be
Colimaters <dmorel-pro@proton.me>
2023-08-20 13:11:18 +0200

    Correct error in get_content_file.c

--- src/get_content_file.c
+++ src/get_content_file.c
@@ -7,7 +7,7 @@
 
 #include "../include/listing_function.h"
 
-char *get_content_file(const char * const file)
+char *get_content_file(const char *const file)
 {
     struct stat sts;
     int fd = open(file, O_RDONLY);
@@ -18,11 +18,13 @@ char *get_content_file(const char * const file)
         return (my_putstr_err("get_content_file() : file was NULL\n"), NULL);
     if (stat(file, &sts) != 0)
         return (my_putstr_err("get_content_file() : stat failed\n"), NULL);
-    buffer = smalloc(sts.st_size);
+    buffer = smalloc(sts.st_size + 1);
     if (!buffer)
         return (my_putstr_err("get_content_file() : buffer was NULL\n"), NULL);
     ret = read(fd, buffer, sts.st_size);
     if (ret == -1)
         return (my_putstr_err("get_content_file() : read failed\n"), NULL);
+    buffer[ret] = '\0';
+    close(fd);
     return (buffer);
 }
